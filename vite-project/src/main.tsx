22922918b424723b9ea82f74822a12d9d901c66f
Playbird12 <83607406+Playbird12@users.noreply.github.com>
2022-05-14 22:51:20 -0500

    added example graphs

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
+import RaceBargraph from './components/raceBargraph'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+
+    <>
+        <App />
+        <RaceBargraph />
+    </>
+ 
 )
