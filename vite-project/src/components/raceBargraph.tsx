e6f5bff74a9c0b349f551c902f2e69f2964ad924
Playbird12 <83607406+Playbird12@users.noreply.github.com>
2022-05-15 01:33:13 -0500

    working, code

--- vite-project/src/components/raceBargraph.tsx
+++ vite-project/src/components/raceBargraph.tsx
@@ -13,47 +13,41 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    name: "Asian",
+    uv: 6969,
+    pv: 4200,
     amt: 2400
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    name: "Latino",
+    uv: 4200,
+    pv: 7272,
     amt: 2210
   },
   {
-    name: "Page C",
+    name: "Aaroh",
     uv: 2000,
     pv: 9800,
     amt: 2290
   },
   {
-    name: "Page D",
+    name: "Tak",
     uv: 2780,
     pv: 3908,
     amt: 2000
   },
   {
-    name: "Page E",
+    name: "Trash",
     uv: 1890,
     pv: 4800,
     amt: 2181
   },
   {
-    name: "Page F",
+    name: "HAHAHA",
     uv: 2390,
     pv: 3800,
     amt: 2500
   },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
 ];
 
 export default function RaceBargraph() {
@@ -76,7 +70,7 @@ export default function RaceBargraph() {
       <Tooltip />
       <Legend />
       <Bar dataKey="pv" fill="#8884d8" />
-      <Bar dataKey="uv" fill="#82ca9d" />
+
     </BarChart>
   );
 }
